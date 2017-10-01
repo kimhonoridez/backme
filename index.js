54a71b31db1785267a043db86f8bbf27fbbd3ef3
DESKTOP-2J2HEQC\KimHonoridez <kimhonoridez@gmail.com>
2017-10-02 01:36:37 +0800

    Add Backend Codes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /**
 	@author: Kim Honoridez
 */
@@ -67,4 +68,85 @@
 
 	app.listen(port);
 
+=======
+/**
+	@author: Kim Honoridez
+*/
+
+(function () {
+	'use strict';
+
+	var express = require('express');
+	var bodyParser = require('body-parser');
+	var https = require('https');
+	var fs = require('fs');
+
+	var app = express();
+	var port = 3000;
+
+	var router = express.Router();
+
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+	app.use('/api', router);
+
+	// START: Data Simulation APIs
+
+	var rows = 2;
+	var columns = 3;
+	var dataList = [];
+
+	router.get('/', function(req, res) {
+		res.header('Access-Control-Allow-Origin', '*');
+
+	    res.json({
+	    	r: rows,
+	    	c: columns,
+	    	data: dataList
+	    });   
+	});
+
+	router.post('/', function(req, res) {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+		res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+	    var data = req.body;
+
+	    if (data.rno !== undefined && data.cno !== undefined && data.data !== undefined) {
+	    	if (!isNaN(data.rno) && !isNaN(data.cno) && !isNaN(data.data)) {
+	    		dataList[data.rno * columns + data.cno] = +data.data;
+
+	    		res.status(200).send({
+			    	r: rows,
+			    	c: columns,
+			    	data: dataList
+			    });
+	    	}
+	    	else {
+				res.status(500).send({
+		    		errorCode: "E_002",
+		    		errorMsg: "Field must be a number."
+		    	});
+	    	}
+	    }
+	    else {
+	    	res.status(500).send({
+	    		errorCode: "E_001",
+	    		errorMsg: "Lacking required field."
+	    	});
+	    }
+	});
+
+	// END: Data Simulation APIs
+
+	//app.listen(port);
+
+	https.createServer({
+      key: fs.readFileSync('./kissaaviota.key'),
+      cert: fs.readFileSync('./kissaaviota.cert')
+    }, app).listen(port);
+
+
+>>>>>>> Stashed changes
 })();
