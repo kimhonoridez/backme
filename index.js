5a2dbf0dcc2222e3dd462f92ed039505fad4254d
kimhonoridez <kimhonoridez@yahoo.com>
2017-10-02 10:33:36 +0800

    Updated files

--- index.js
+++ index.js
@@ -1,74 +1,3 @@
-<<<<<<< Updated upstream
-/**
-	@author: Kim Honoridez
-*/
-
-(function () {
-	'use strict';
-
-	var express = require('express');
-	var bodyParser = require('body-parser');
-	var cors = require('cors');
-
-	var app = express();
-	var port = 8080;
-
-	var router = express.Router();
-
-	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(bodyParser.json());
-	app.use('/api', router);
-
-	// START: Data Simulation APIs
-
-	var rows = 2;
-	var columns = 3;
-	var dataList = [];
-
-	router.get('/', function(req, res) {
-		req.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Origin', '*');
-
-	    res.json({
-	    	r: rows,
-	    	c: columns,
-	    	data: dataList
-	    });   
-	});
-
-	router.post('/', function(req, res) {
-	    var data = req.body;
-
-	    if (data.rno !== undefined && data.cno !== undefined && data.data !== undefined) {
-	    	if (!isNaN(data.rno) && !isNaN(data.cno) && !isNaN(data.data)) {
-	    		dataList[data.rno * columns + data.cno] = +data.data;
-
-	    		res.status(200).json({
-			    	r: rows,
-			    	c: columns,
-			    	data: dataList
-			    });
-	    	}
-	    	else {
-				res.status(500).send({
-		    		errorCode: "E_002",
-		    		errorMsg: "Field must be a number."
-		    	});
-	    	}
-	    }
-	    else {
-	    	res.status(500).send({
-	    		errorCode: "E_001",
-	    		errorMsg: "Lacking required field."
-	    	});
-	    }
-	});
-
-	// END: Data Simulation APIs
-
-	app.listen(port);
-
-=======
 /**
 	@author: Kim Honoridez
 */
@@ -85,6 +14,9 @@
 	var port = 3000;
 
 	var router = express.Router();
+	
+	var cors = require('cors');
+	app.use(cors());
 
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
@@ -97,8 +29,6 @@
 	var dataList = [];
 
 	router.get('/', function(req, res) {
-		res.header('Access-Control-Allow-Origin', '*');
-
 	    res.json({
 	    	r: rows,
 	    	c: columns,
@@ -107,10 +37,6 @@
 	});
 
 	router.post('/', function(req, res) {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-		res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-
 	    var data = req.body;
 
 	    if (data.rno !== undefined && data.cno !== undefined && data.data !== undefined) {
@@ -146,7 +72,4 @@
       key: fs.readFileSync('./kissaaviota.key'),
       cert: fs.readFileSync('./kissaaviota.cert')
     }, app).listen(port);
-
-
->>>>>>> Stashed changes
 })();
